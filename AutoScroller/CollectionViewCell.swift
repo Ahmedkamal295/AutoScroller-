cdbbdcb7ec3185991741092b5c1be474a7c04501
Ahmedkamal295 <Ahmedkamal21995@gmail.com>
2020-05-07 13:58:15 -0700

    first commit

---
+++ AutoScroller/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  AutoScroller
+//
+//  Created by Ahmed on 3/5/20.
+//  Copyright © 2020 Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
+}
---
+++ Pods/Target Support Files/Pods-AutoScroller/Pods-AutoScroller-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AutoScroller : NSObject
+@end
+@implementation PodsDummy_Pods_AutoScroller
+@end
